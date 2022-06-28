0246bf6de541487df3aa6e55164eb69bd823514a
Dani BL <danibachsl@protonmail.com>
2022-06-28 20:30:11 +0200

    added changes

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,4 +3,23 @@ const router = require("express").Router();
 
 // all your routes here
 
+router.get('/movies/create', (req, res, next) => {
+    res.render('movies/new-movie.hbs')
+})
+
+router.post('/movies/create', (req, res, next) => {
+    const { movie } = req.body;
+    User.findOne({ movie })
+      .then((userDocFromDB) => {
+        if (!userDocFromDB) {
+          // prettier-ignore
+          User.create({ movie })
+          .then(() => res.redirect('/movies/create'));
+        } else {
+          res.render("/movies/create", { message: "It seems the movie already exists. ☀️" });
+          return;
+        }
+      })
+      .catch((err) => console.log(`Error while creating a new movie: ${err}`));
+})
 module.exports = router;
