e4ac0a75454055cdf60c27b4782e141e1d963681
Dani BL <danibachsl@protonmail.com>
2022-06-28 15:39:07 +0200

    iteración 1

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,25 @@
+const Celebrity = require("../models/Celebrity.model");
+
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
 // all your routes here
+
+router.get('/celebrities/create', (req, res, next) => {
+    res.render('celebrities/new-celebrity')
+})
+router.post('/celebrities/create', (req, res, next) => {
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+    const catchPhrase = req.body.catchPhrase:
+
+    Celebrity.create({name:name, occupation:occupation, catchPhrase:catchPhrase})
+    .then(() => {
+        res.redirect("/celebrities")
+    })
+    .catch(() => {
+        res.redirect('celebrities/new-celebrity')
+    })
+});
 
 module.exports = router;
